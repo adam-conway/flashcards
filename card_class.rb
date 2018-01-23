cb5b5876f801fa24fe23781cf4f26d38a6b4fb37
adam-conway <adam.n.conway@gmail.com>
2018-01-23 16:11:07 -0700

    Added attr_reader to Card class

--- card_class.rb
+++ card_class.rb
@@ -1,20 +1,14 @@
-require card_clas_test.
+class Card
+  attr_reader :question,
+              :answer
 
-#class Card
   def initialize(question,answer)
     @question = question
     @answer = answer
   end
 
-  def questions
-    puts @question
-  end
-
-  def answer
-    puts @answer
-  end
 end
 
 card = Card.new("What is the capital of Alaska?", "Juneau")
-card.question
-card.answer
+puts card.question
+puts card.answer
