6461d0b47cf21a5e80ff19305f75b00b58239444
adam-conway <adam.n.conway@gmail.com>
2018-01-25 10:23:18 -0700

    Finished iteration 1 with tests

--- lib/card_class.rb
+++ lib/card_class.rb
@@ -1,10 +1,16 @@
 require 'pry'
 
 class Card
-  attr_reader :question
+  attr_reader :question,
+              :answer
 
   def initialize(question,answer)
     @question = question
+    @answer = answer
   end
 
 end
+
+card = Card.new("What is the capital of Alaska?", "Juneau")
+puts card.question
+puts card.answer
--- test/card_class_test.rb
+++ test/card_class_test.rb
@@ -4,9 +4,14 @@ require 'minitest/pride'
 require './lib/card_class.rb'
 
 class CardTest < Minitest::Test
-  def test_card_class_exists
-    card = Card.new("Testing", "Stuff")
+  def test_card_class_initializes
+    card = Card.new("What is the capital of Alaska?", "Juneau")
     assert_instance_of Card, card
-    assert_equal :question, "Testing"
+  end
+
+  def test_card_variables_set
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+    assert_equal card.question, "What is the capital of Alaska?"
+    assert_equal card.answer, "Juneau"
   end
 end
