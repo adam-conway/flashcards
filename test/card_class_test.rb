2044e5082678b481337854d17a8cfdab10d44f76
adam-conway <adam.n.conway@gmail.com>
2018-01-25 08:21:22 -0700

    Cleaning up file structure

--- test/card_class_test.rb
+++ test/card_class_test.rb
@@ -1,10 +1,12 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/card_class.rb'
 
 class CardTest < Minitest::Test
-  def card_class_exists
-    card = Card.new
+  def test_card_class_exists
+    card = Card.new("Testing", "Stuff")
     assert_instance_of Card, card
+    assert_equal :question, "Testing"
   end
 end
