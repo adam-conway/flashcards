9616fc2ca70e649cc953379a379965f0e99d9f86
adam-conway <adam.n.conway@gmail.com>
2018-01-28 15:12:30 -0700

    Allowing user to enter filename for cards to use

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -5,9 +5,17 @@ require './lib/guess_class.rb'
 require './lib/round_class.rb'
 require './lib/card_generator_class.rb'
 
-filename = "./lib/cards.txt"
+# filename = "./lib/cards.txt"
+puts "Please let me know where your questions are:"
+filename = gets.chomp
+
+until File.exists?(filename)
+  puts "Sorry, that file does not exist. Please specify a new file:"
+  filename = gets.chomp
+end
+
+
 list_of_cards = CardGenerator.new(filename).cards
 deck = Deck.new(list_of_cards)
 round = Round.new(deck)
-
 round.start
